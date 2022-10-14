9e62e13d007e7248fd67d904cce5daa26697d79c
samriddhikashyap <109653749+samriddhikashyap@users.noreply.github.com>
2022-10-14 20:33:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+prediction_1 = "";
+
+Webcam.set({
+width: 350,
+Height: 300,
+image_format: 'png',
+png_quality: 90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+Webcam.snap(function (data_uri) {
+document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>'
+});
+}
+console.log('ml5 version:', ml5.version);
+classifier = ml5.imageClassifier('', modelLoaded);
+
+function modelLoaded(){
+console.log('model loaded!');
+}
+
+function speak(){
+var synth = window.speechSynthesis;
+speak_data_1 = "The first prediction is " + prediction_1;
+var utterThis = new SpeechSynthesisUtterance(speak_data_1);
+utterThis.rate = 0.5;
+synth.speak(utterThis);
+}
+
+function check()
+{
+img = document.getElementById('captured_image');
+classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+if (error)
+{
+console.error(error);
+} else {
+console.log(results);
+document.getElementById("result_gesture_name").innerHTML = results[0].label;
+prediction_1 = results[0].label;
+speak();
+}
+}
